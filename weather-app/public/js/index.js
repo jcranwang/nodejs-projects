888207172f8c7752b331c7195736ff298a64a6e8
jcranwang <jason.cr.wang@gmail.com>
2019-04-26 01:38:56 +1000

    Weather app: setup search form and finish the app

--- weather-app/public/js/index.js
+++ weather-app/public/js/index.js
@@ -1,20 +1,28 @@
+const form = document.querySelector("form");
+const searchBar = document.querySelector(".search");
 const locationText = document.querySelector(".location");
 const summaryText = document.querySelector(".summary");
 const tempText = document.querySelector(".temp");
 const errorText = document.querySelector(".error");
 
-fetch("http://localhost:3000/weather?address=Adelaide")
-  .then(response => {
-    locationText.textContent = "Loading...";
-    return response.json();
-  })
-  .then(({ location, summary, temp, error }) => {
-    if (error) {
-      locationText.textContent = "";
-      errorText.textContent = error;
-    } else {
-      locationText.textContent = location;
-      summaryText.textContent = summary;
-      tempText.textContent = `Temperature: ${temp}`;
-    }
-  });
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  const searchTerm = searchBar.value;
+  locationText.textContent = "Loading...";
+  summaryText.textContent = "";
+  tempText.textContent = "";
+  errorText.textContent = "";
+
+  fetch(`http://localhost:3000/weather?address=${searchTerm}`)
+    .then(response => response.json())
+    .then(({ location, summary, temp, error }) => {
+      if (error) {
+        locationText.textContent = "";
+        errorText.textContent = error;
+      } else {
+        locationText.textContent = location;
+        summaryText.textContent = summary;
+        tempText.textContent = `Temperature: ${temp}°C`;
+      }
+    });
+});
